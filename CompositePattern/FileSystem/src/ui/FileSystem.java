e59d51e29df62e43bee0f3704ef7232424026cec
Achilles Moraites <achimoraites@yahoo.gr>
2017-11-29 12:32:17 +0200

    made some additions

--- CompositePattern/FileSystem/src/ui/FileSystem.java
+++ CompositePattern/FileSystem/src/ui/FileSystem.java
@@ -3,23 +3,26 @@ package ui;
 import model.File;
 import model.Folder;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class FileSystem {
     public static void main(String[] args) {
-        List<Folder> folders = new ArrayList<>();
+
         Folder f1 = new Folder("Folder 1:");
         Folder f2 = new Folder("Folder 2:");
+        Folder f3 = new Folder("Folder 3:");
 
         File file1 = new File("File 1" ,"Data of file 1");
         File file2 = new File("File 2" ,"Data of file 2");
         File file3 = new File("File 3" ,"Data of file 3");
+        File file4 = new File("File 4" ,"Data of file 4");
+        File file5 = new File("File 5" ,"Data of file 5");
 
         f1.addFileComponent(file1);
         f1.addFileComponent(file2);
         f1.addFileComponent(f2);
         f2.addFileComponent(file3);
+        f2.addFileComponent(f3);
+        f3.addFileComponent(file4);
+        f3.addFileComponent(file5);
 
         f1.display(" ");
     }
